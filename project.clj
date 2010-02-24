14e2ff372fb98f1366a6551ab0f1c2452838653b
Seth Buntin <seth.buntin@gmail.com>
2010-02-24 09:53:57 -0600

    update karras dependency to 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Humongous: a Clojure MongoDB web interface"
   :dependencies [[org.buntin/compojure "0.4.0-SNAPSHOT"]
                  [hiccup "0.1"]
-                 [karras "0.1.0-SNAPSHOT"]
+                 [karras "0.2.0-SNAPSHOT"]
                  [org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [ring "0.1.1-SNAPSHOT"]]
