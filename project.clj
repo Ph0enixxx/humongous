6aff831fa221600501fe823e8a86719538afcbb2
Seth Buntin <seth.buntin@gmail.com>
2010-02-24 07:46:07 -0600

    use org.buntin/compojure from clojars until compojure releases

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject humongous "0.1.0-SNAPSHOT"
   :description "Humongous: a Clojure MongoDB web interface"
-  :dependencies [[compojure "0.4.0-SNAPSHOT"]
+  :dependencies [[org.buntin/compojure "0.4.0-SNAPSHOT"]
                  [hiccup "0.1"]
                  [karras "0.1.0-SNAPSHOT"]
                  [org.clojure/clojure "1.1.0"]
